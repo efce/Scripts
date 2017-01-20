c8ea53bb30d57c2e2ad304803d64559d48419742
Filip Ciepiela <filip.ciepiela@gmail.com>
2017-01-20 11:02:46 +0100

    add info

--- getPM2.5.php
+++ getPM2.5.php
@@ -1,4 +1,5 @@
 <?php
+$pm25dopuszczalny = 25;
 $url = 'http://monitoring.krakow.pios.gov.pl/dane-pomiarowe/pobierz';
 //TODO: WTF is channels ?
 $data = array('query' => '{"measType":"Auto","viewType":"Station","dateRange":"Day","date":"' . date('d.m.Y') . '","viewTypeEntityId":6,"channels":[1711,44,46,202,43,42,45]}');
@@ -24,7 +25,7 @@ foreach ( $dane->data as $pdatas ) {
 		foreach ($pdatas as $pdata) {
 			if ( isset($pdata->paramCode) && $pdata->paramCode == 'PM2.5' ) {
 				$tmp = end($pdata->data);
-				echo 'PM2.5 Concentration: ' . $tmp[1] . ' ' . $pdata->unit . ', date: ' . date('Y-m-d H:i:s',$tmp[0]) . "\n";
+				echo 'C_PM2.5: ' . number_format($tmp[1],0) . ' ' . $pdata->unit . ' (' . number_format($tmp[1]/$pm25dopuszczalny*100,0) . '%), ' . date('Y-m-d H:i',$tmp[0]) . "\n";
 				exit;
 			}
 
